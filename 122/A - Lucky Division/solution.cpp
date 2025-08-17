6ac8b1dc71278dfab7292ea240bd3899dcb1b7e6
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2025-08-17 08:46:31 +0530

    Updated solution.cpp

--- 122/A - Lucky Division/solution.cpp
+++ 122/A - Lucky Division/solution.cpp
@@ -1,1 +1,42 @@
-#include <bits/stdc++.h>using namespace std; #define ll              long long#define mod             1e9 + 7#define inf             9223372036854775807#define ninf            -9223372036854775808#define vec             vector<ll>#define vecStr          vector<string>#define vecChr          vector<char>#define vecBool         vector<bool>#define vec2d           vector<vector<ll>>#define vecP            vector<pair<ll, ll>>#define queLl           queue<ll>#define queP            queue<pair<ll, ll>>#define unmp_ll_ll      unordered_map<ll, ll>#define unmp_chr_ll     unordered_map<char, ll>#define unmp_str_ll     unordered_map<string, ll>#define pb              push_back#define pp              pop_back#define ff              first#define ss              second#define all(x)          (x).begin(), (x).end()#define rall(x)         (x).rbegin(), (x).rend()#define sz(x)           (int)(x).size() void solve() {    ll l;    cin >> l;     if (l % 4 == 0 || l % 7 == 0 || l % 47 == 0 || l % 74 == 0 || l % 477 == 0)        cout << "YES" << endl;    else        cout << "NO" << endl;} int main() {    while (true) {        solve();        break;    }}
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll              long long
+#define mod             1e9 + 7
+#define inf             9223372036854775807
+#define ninf            -9223372036854775808
+#define vec             vector<ll>
+#define vecStr          vector<string>
+#define vecChr          vector<char>
+#define vecBool         vector<bool>
+#define vec2d           vector<vector<ll>>
+#define vecP            vector<pair<ll, ll>>
+#define queLl           queue<ll>
+#define queP            queue<pair<ll, ll>>
+#define unmp_ll_ll      unordered_map<ll, ll>
+#define unmp_chr_ll     unordered_map<char, ll>
+#define unmp_str_ll     unordered_map<string, ll>
+#define pb              push_back
+#define pp              pop_back
+#define ff              first
+#define ss              second
+#define all(x)          (x).begin(), (x).end()
+#define rall(x)         (x).rbegin(), (x).rend()
+#define sz(x)           (int)(x).size()
+
+void solve() {
+    ll l;
+    cin >> l;
+
+    if (l % 4 == 0 || l % 7 == 0 || l % 47 == 0 || l % 74 == 0 || l % 477 == 0)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
+
+int main() {
+    while (true) {
+        solve();
+        break;
+    }
+}
