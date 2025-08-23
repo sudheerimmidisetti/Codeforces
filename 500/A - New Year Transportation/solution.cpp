fbe3bfdc1a545701d5cc860a709673ae3cf0fccb
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2025-08-23 11:24:56 +0530

    Updated solution.cpp

--- 500/A - New Year Transportation/solution.cpp
+++ 500/A - New Year Transportation/solution.cpp
@@ -1,1 +1,56 @@
-#include <bits/stdc++.h>using namespace std; #define ll              long long#define mod             1e9 + 7#define inf             9223372036854775807#define ninf            -9223372036854775808#define vec             vector<ll>#define vecStr          vector<string>#define vecChr          vector<char>#define vecBool         vector<bool>#define vec2d           vector<vector<ll>>#define vecP            vector<pair<ll, ll>>#define queLl           queue<ll>#define queP            queue<pair<ll, ll>>#define unmp_ll_ll      unordered_map<ll, ll>#define unmp_chr_ll     unordered_map<char, ll>#define unmp_str_ll     unordered_map<string, ll>#define pb              push_back#define pp              pop_back#define ff              first#define ss              second#define all(x)          (x).begin(), (x).end()#define rall(x)         (x).rbegin(), (x).rend()#define sz(x)           (int)(x).size() void solve() {    ll n, t;    cin >> n >> t;     vec a(n);    for (int i = 0; i < n - 1; i++) cin >> a[i];     bool can = false;    for (int i = 0; i < n - 1;) {        i += a[i];         if ((i + 1) == t) {            can = true;            break;        } else if (i > t || i >= n - 1)            break;    }     if (can)        cout << "YES" << endl;    else        cout << "NO" << endl;} int main() {    while (true) {        solve();        break;    }}
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll              long long
+#define mod             1e9 + 7
+#define inf             9223372036854775807
+#define ninf            -9223372036854775808
+#define vec             vector<ll>
+#define vecStr          vector<string>
+#define vecChr          vector<char>
+#define vecBool         vector<bool>
+#define vec2d           vector<vector<ll>>
+#define vecP            vector<pair<ll, ll>>
+#define queLl           queue<ll>
+#define queP            queue<pair<ll, ll>>
+#define unmp_ll_ll      unordered_map<ll, ll>
+#define unmp_chr_ll     unordered_map<char, ll>
+#define unmp_str_ll     unordered_map<string, ll>
+#define pb              push_back
+#define pp              pop_back
+#define ff              first
+#define ss              second
+#define all(x)          (x).begin(), (x).end()
+#define rall(x)         (x).rbegin(), (x).rend()
+#define sz(x)           (int)(x).size()
+
+void solve() {
+    ll n, t;
+    cin >> n >> t;
+
+    vec a(n);
+    for (int i = 0; i < n - 1; i++) cin >> a[i];
+
+    bool can = false;
+    for (int i = 0; i < n - 1;) {
+        i += a[i];
+
+        if ((i + 1) == t) {
+            can = true;
+            break;
+        } else if (i > t || i >= n - 1)
+            break;
+    }
+
+    if (can)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
+
+int main() {
+    while (true) {
+        solve();
+        break;
+    }
+}
