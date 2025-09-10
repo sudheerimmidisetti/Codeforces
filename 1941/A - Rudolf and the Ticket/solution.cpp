4ee4622fe9c064fbe939e3dd20240f5efa53d2a4
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2025-09-10 19:34:22 +0530

    Updated solution.cpp

--- 1941/A - Rudolf and the Ticket/solution.cpp
+++ 1941/A - Rudolf and the Ticket/solution.cpp
@@ -1,1 +1,65 @@
-#include <bits/stdc++.h>using namespace std; #define ll              long long#define mod             1e9 + 7#define inf             9223372036854775807#define ninf            -9223372036854775808#define vec             vector<ll>#define vecStr          vector<string>#define vecChr          vector<char>#define vecBool         vector<bool>#define vec2d           vector<vector<ll>>#define vecP            vector<pair<ll, ll>>#define queLl           queue<ll>#define queP            queue<pair<ll, ll>>#define unmp_ll_ll      unordered_map<ll, ll>#define unmp_ll_chr     unordered_map<ll, char>#define unmp_chr_ll     unordered_map<char, ll>#define unmp_str_ll     unordered_map<string, ll>#define unmp_str_str    unordered_map<string, string>#define stChr           set<char>#define pb              push_back#define pp              pop_back#define ff              first#define ss              second#define all(x)          (x).begin(), (x).end()#define rall(x)         (x).rbegin(), (x).rend()#define rev(x)          (x).begin(), (x).end()#define sz(x)           (int)(x).size() vec arrayCreation(ll n) {    vec a(n);    for (auto &x : a) cin >> x;     return a;} void solve() {    ll n, m, k;    cin >> n >> m >> k;     vec b = arrayCreation(n);    sort(all(b));        vec c = arrayCreation(m);    sort(all(c));     ll selectionCnt = 0;    for (auto &x : b) {        for (auto &y : c) {            if ((x + y) <= k)                selectionCnt++;        }    }     cout << selectionCnt << endl;} int main() {    ll t = 1;    cin >> t;     while (t--)        solve();}
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll              long long
+#define mod             1e9 + 7
+#define inf             9223372036854775807
+#define ninf            -9223372036854775808
+#define vec             vector<ll>
+#define vecStr          vector<string>
+#define vecChr          vector<char>
+#define vecBool         vector<bool>
+#define vec2d           vector<vector<ll>>
+#define vecP            vector<pair<ll, ll>>
+#define queLl           queue<ll>
+#define queP            queue<pair<ll, ll>>
+#define unmp_ll_ll      unordered_map<ll, ll>
+#define unmp_ll_chr     unordered_map<ll, char>
+#define unmp_chr_ll     unordered_map<char, ll>
+#define unmp_str_ll     unordered_map<string, ll>
+#define unmp_str_str    unordered_map<string, string>
+#define stChr           set<char>
+#define pb              push_back
+#define pp              pop_back
+#define ff              first
+#define ss              second
+#define all(x)          (x).begin(), (x).end()
+#define rall(x)         (x).rbegin(), (x).rend()
+#define rev(x)          (x).begin(), (x).end()
+#define sz(x)           (int)(x).size()
+
+vec arrayCreation(ll n) {
+    vec a(n);
+    for (auto &x : a) cin >> x;
+
+    return a;
+}
+
+void solve() {
+    ll n, m, k;
+    cin >> n >> m >> k;
+
+    vec b = arrayCreation(n);
+    sort(all(b));
+    
+    vec c = arrayCreation(m);
+    sort(all(c));
+
+    ll selectionCnt = 0;
+    for (auto &x : b) {
+        for (auto &y : c) {
+            if ((x + y) <= k)
+                selectionCnt++;
+        }
+    }
+
+    cout << selectionCnt << endl;
+}
+
+int main() {
+    ll t = 1;
+    cin >> t;
+
+    while (t--)
+        solve();
+}
