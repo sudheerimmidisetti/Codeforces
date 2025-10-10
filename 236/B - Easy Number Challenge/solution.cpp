8030dd9d7947b3eba4c032d62b736fe16f2a4f96
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2025-10-10 21:00:12 +0530

    Updated solution.cpp

--- 236/B - Easy Number Challenge/solution.cpp
+++ 236/B - Easy Number Challenge/solution.cpp
@@ -1,1 +1,86 @@
-#include <bits/stdc++.h>using namespace std; using ll                = long long;using vec               = vector<ll>;using vec2d             = vector<vec>;using vecStr            = vector<string>;using vecStr2d          = vector<vecStr>;using vecChr            = vector<char>;using vecChr2d          = vector<vecChr>;using vecBool           = vector<bool>;using vecP_ll_ll        = vector<pair<ll, ll>>;using vecP_ll_str       = vector<pair<ll, string>>;using unmp_ll_ll        = unordered_map<ll, ll>;using unmp_chr_ll       = unordered_map<char, ll>;using unmp_str_ll       = unordered_map<string, ll>;using st_ll             = set<ll>;using st_chr            = set<char>;using st_str            = set<string>;using que_ll            = queue<ll>;using queP              = queue<pair<ll, ll>>; #define pb              push_back#define pp              pop_back#define ff              first#define ss              second#define all(x)          (x).begin(), (x).end()#define rall(x)         (x).rbegin(), (x).rend()#define sm(x, y, z)     (x).begin() + y, (x).begin() + z#define sz(x)           (int)(x).size()#define nl              endl const ll MOD            = 1e9 + 7;const ll INF            = LLONG_MAX;const ll NINF           = LLONG_MIN; vecBool isPrime(1e6 + 10, true); void generatePrimes() {    isPrime[0] = isPrime[1] = false;    for (int i = 2; i * i <= (ll)(1e6 + 10); i++) {        if (isPrime[i]) {            for (int j = i * i; j <= (ll)(1e6 + 10); j += i)                isPrime[j] = false;        }    }} vec divisorsCnt(1e6 + 1, 0); void getDivisors() {    for (ll i = 1; i <= (ll)(1e6 + 1); i++)        for (ll j = i; j <= (ll)(1e6 + 1); j += i)            divisorsCnt[j]++;} vec arrayCreation(ll n) {    vec a(n);    for (auto& x: a)        cin >> x;     return a;} void solve() {    ll a, b, c;    cin >> a >> b >> c;        ll sum = 0;    for(int i = 1; i <= a; i++)        for(int j = 1; j <= b; j++)            for(int k = 1; k <= c; k++)                sum = (sum + divisorsCnt[i * j * k]) % 1073741824;        cout << sum;} int main() {    ll t = 1;    // cin >> t;     // generatePrimes();    getDivisors();    while (t--)        solve();}
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll                = long long;
+using vec               = vector<ll>;
+using vec2d             = vector<vec>;
+using vecStr            = vector<string>;
+using vecStr2d          = vector<vecStr>;
+using vecChr            = vector<char>;
+using vecChr2d          = vector<vecChr>;
+using vecBool           = vector<bool>;
+using vecP_ll_ll        = vector<pair<ll, ll>>;
+using vecP_ll_str       = vector<pair<ll, string>>;
+using unmp_ll_ll        = unordered_map<ll, ll>;
+using unmp_chr_ll       = unordered_map<char, ll>;
+using unmp_str_ll       = unordered_map<string, ll>;
+using st_ll             = set<ll>;
+using st_chr            = set<char>;
+using st_str            = set<string>;
+using que_ll            = queue<ll>;
+using queP              = queue<pair<ll, ll>>;
+
+#define pb              push_back
+#define pp              pop_back
+#define ff              first
+#define ss              second
+#define all(x)          (x).begin(), (x).end()
+#define rall(x)         (x).rbegin(), (x).rend()
+#define sm(x, y, z)     (x).begin() + y, (x).begin() + z
+#define sz(x)           (int)(x).size()
+#define nl              endl
+
+const ll MOD            = 1e9 + 7;
+const ll INF            = LLONG_MAX;
+const ll NINF           = LLONG_MIN;
+
+vecBool isPrime(1e6 + 10, true);
+
+void generatePrimes() {
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i * i <= (ll)(1e6 + 10); i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= (ll)(1e6 + 10); j += i)
+                isPrime[j] = false;
+        }
+    }
+}
+
+vec divisorsCnt(1e6 + 1, 0);
+
+void getDivisors() {
+    for (ll i = 1; i <= (ll)(1e6 + 1); i++)
+        for (ll j = i; j <= (ll)(1e6 + 1); j += i)
+            divisorsCnt[j]++;
+}
+
+vec arrayCreation(ll n) {
+    vec a(n);
+    for (auto& x: a)
+        cin >> x;
+
+    return a;
+}
+
+void solve() {
+    ll a, b, c;
+    cin >> a >> b >> c;
+    
+    ll sum = 0;
+    for(int i = 1; i <= a; i++)
+        for(int j = 1; j <= b; j++)
+            for(int k = 1; k <= c; k++)
+                sum = (sum + divisorsCnt[i * j * k]) % 1073741824;
+    
+    cout << sum;
+}
+
+int main() {
+    ll t = 1;
+    // cin >> t;
+
+    // generatePrimes();
+    getDivisors();
+    while (t--)
+        solve();
+}
